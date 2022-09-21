513b8b2f6c239fa93f47290127d4e2348beff213
celiorscosta <celiorscosta@hotmai.com>
2022-09-21 10:25:18 -0300

    Finalizando o projeto.

--- src/models/matriculas.js
+++ src/models/matriculas.js
@@ -3,9 +3,14 @@ module.exports = (sequelize, DataTypes) => {
   const Matriculas = sequelize.define('Matriculas', {
     status: DataTypes.STRING
   }, {});
-  Matriculas.associate = function(models) {
-    Matriculas.belongsTo(models.Pessoas, { foreignKey: 'docente_id' });
-    Matriculas.belongsTo(models.Turmas, { foreignKey: 'turma_id' });
+  Matriculas.associate = function (models) {
+    Matriculas.belongsTo(models.Pessoas, {
+      foreignKey: 'estudante_id'
+    })
+    Matriculas.belongsTo(models.Turmas, {
+      foreignKey: 'turma_id'
+    })
+
   };
   return Matriculas;
 };
